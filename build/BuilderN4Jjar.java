de8aaf451b7cdffd5de49a62519e11923204d9d5
lorenzo puccetti <lorenzo.puccetti@gmail.com>
2016-11-12 10:32:44 +0000

    restructuring

---
+++ build/BuilderN4Jjar.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
+
+/**
+ * Compiles N4J.java and zips the class into a n4j.jar
+**/
+public class BuilderN4Jjar {
+    public static void main(String args[]) throws Exception {
+        System.out.println("BuilderN4Jjar");
+        System.out.println("=============");
+        System.out.println();
+        
+        File fileIn =  new File(System.getProperty("user.dir")+"/src/N4J.java"); 
+        File fileOut = new File(System.getProperty("user.dir")+"/docs/dist/n4j.jar");
+
+        System.out.println("Compiling " + fileIn);
+        String code = load(fileIn);
+        ByteArrayOutputStream baosN4Jclazz = new ByteArrayOutputStream();
+        JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
+        javac.getTask(null,
+                new ForwardingJavaFileManager<JavaFileManager>(javac.getStandardFileManager(null, null, null)) {
+                    @Override
+                    public JavaFileObject getJavaFileForOutput(JavaFileManager.Location location, String className,
+                            JavaFileObject.Kind kind, FileObject sibling) {
+                        return new SimpleJavaFileObject(URI.create(""), JavaFileObject.Kind.CLASS) {
+                            @Override
+                            public OutputStream openOutputStream() {
+                                return baosN4Jclazz;
+                            }
+                        };
+                    }
+                }, null, Arrays.asList(new String[] { "-g:none" }), null,
+                Arrays.asList(new SimpleJavaFileObject(
+                        URI.create("string:///N4J.java"),
+                        JavaFileObject.Kind.SOURCE) {
+                    public CharSequence getCharContent(boolean b) {
+                        return code;
+                    }
+                })).call();
+
+        ByteArrayOutputStream baosJAR = new ByteArrayOutputStream();
+
+        JarOutputStream jar = new JarOutputStream(baosJAR);
+        JarEntry entry = new JarEntry("N4J.class");
+        entry.setTime(fileIn.lastModified());
+        jar.putNextEntry(entry);
+        jar.write(baosN4Jclazz.toByteArray());
+        jar.closeEntry();
+        jar.close();
+
+        FileOutputStream fos = new FileOutputStream(fileOut);
+        fos.write(baosJAR.toByteArray());
+        fos.close();
+
+        System.out.println("N4J.class (size) " + baosN4Jclazz.toByteArray().length);
+        System.out.println("N4J.jar   (size) " + baosJAR.toByteArray().length);
+        System.out.println("done");
+    }
+
+    private static String load(File f) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String line = br.readLine();
+            if (line == null) break;
+            sb.append(line);
+            sb.append('\n');
+        }
+        br.close();
+        return sb.toString();
+    }
+}
---
+++ src/N4J.java
@@ -0,0 +1,69 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+/**
+ * N4J: the only java class in n4j.jar 
+ * It has a static method 'start' to launch a minimal HttpServer capable of 
+ * 1. returning the landing page (an HTML page which just wraps the script n4j.js to create the web console)
+ * 2. handling http posts /runJS to execute nashorn snippets and return the .toString()   
+ *  
+**/
+public final class N4J implements HttpHandler {
+	
+	private ScriptEngine engine;
+	
+	public N4J(ScriptEngine engine) {
+		this.engine = engine;
+	}
+	
+	public void handle(final HttpExchange  httpExchange) throws IOException {
+        final String uri = httpExchange.getRequestURI().toString();
+        if (uri.equals("/runJS")) {
+            final InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), UTF8);
+            final Object output; 
+            try {
+            	output = engine.eval(isr);
+            } catch (final Exception e) {
+                final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                baos.write("$error:".getBytes(UTF8));
+                e.printStackTrace(new PrintStream(baos));
+                write(httpExchange, baos.toByteArray(),"text/plain");
+                return; 
+            }
+            write(httpExchange,(output+"").getBytes(UTF8),"text/plain");
+            return; 
+        }
+        write(httpExchange,landingPage,"text/html");
+    }
+	
+	private static final Charset UTF8 = Charset.forName("UTF-8");
+    private static final byte[] landingPage = "<!doctype html><script src=https://lorenzoongithub.github.io/nudge4j/dist/console.html.js></script>".getBytes(UTF8);
+    
+    public static final void start(final int port, final Object... args) throws IOException {
+    	final HttpServer server = HttpServer.create(new InetSocketAddress(port), 0); // fail fast.
+        final ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript"); 
+        engine.put("args", args);
+        server.createContext("/", new N4J(engine));
+        server.setExecutor(null);  
+        server.start();
+        System.out.println("nudge4j serving on port:"+port);
+    }
+	
+    private static final void write(final HttpExchange httpExchange, final byte[] array, final String contentType) throws IOException {
+        httpExchange.getResponseHeaders().set("Content-Type", contentType);
+        httpExchange.sendResponseHeaders(200, array.length);
+        final OutputStream os = httpExchange.getResponseBody();
+        os.write(array);
+        os.close();
+    }
+}
